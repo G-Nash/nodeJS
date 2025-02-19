82f0e6fdece381767feaa4f18beb85cfa9390012
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-19 08:37:08 +0530

    class

--- 19thfeb/main.js
+++ 19thfeb/main.js
@@ -1,15 +1,15 @@
-const http =require("http")
+const http = require("http");
 
-let obj={name: "nash", city: "Neverland"};
+let obj = { name: "nash", city: "Neverland" };
 
-const server=http.createServer((req,res)=>{
-    res.writeHead(201, "response received",{
-        "conten-type": "application/json",
+const server = http.createServer((req, res) => {
+    res.writeHead(201, "response received", {
+        "Content-Type": "application/json",
     });
-    res.write(JSON.stringigy(obj));
+    res.write(JSON.stringify(obj)); 
     res.end();
-})
+});
 
-server.listen("3101",()=>{
-    console.log("server running")
-})
+server.listen(3101, () => {
+    console.log("server running on port 3101");
+});
