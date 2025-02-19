b169341cf006f761de870b105afbb1d783404cd4
G-Nash <20b91a5725@srkrec.ac.in>
2025-02-19 08:47:17 +0530

    class commit

--- 19thfeb/main.js
+++ 19thfeb/main.js
@@ -3,11 +3,42 @@ const http = require("http");
 let obj = { name: "nash", city: "Neverland" };
 
 const server = http.createServer((req, res) => {
-    res.writeHead(201, "response received", {
-        "Content-Type": "application/json",
-    });
-    res.write(JSON.stringify(obj)); 
-    res.end();
+    console.log(req.method)
+    console.log(req.url)
+
+    if (req.method =="GET"){
+
+        if(req.url=="/home"){
+            res.writeHead(201,"tryna access home huhh?", {
+                "content-type":"application/json"
+            })
+            res.write("well, here it is...your home") 
+            res.end();
+
+        }
+
+        else if(req.url=="/about"){
+            res.writeHead(201,"tryna access about now huhh?", {
+                "content-type":"application/json"
+            })
+            res.write("well, here it is...your about") 
+            res.end();
+
+        }
+
+        else{
+            res.writeHead(201, "response received", {
+                "Content-Type": "application/json",
+            });
+            res.write(JSON.stringify(obj)); 
+            res.end();
+        }
+    }
+
+    else{
+        res.end("invalid request")
+    }
+
 });
 
 server.listen(3101, () => {
