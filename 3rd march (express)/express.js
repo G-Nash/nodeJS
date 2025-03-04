b0433e256852c44df28f8d50897003d6fda757c0
G-Nash <20b91a5725@srkrec.ac.in>
2025-03-04 11:39:09 +0530

    4th march

--- 3rd march (express)/express.js
+++ 3rd march (express)/express.js
@@ -1,1 +1,17 @@
 //some stuff about file system and getting
+
+//fs module
+//file system module
+
+//CRUD-create, read, update and Delete
+
+
+//require("fs")  -->  it will import the fs module to the project.
+
+//fs.readfile() ---> is asynchronous
+//fs.readfilesync() ---> is synchronous
+//fs.writefile() ---> is asynchronous
+//fs.writefilesync() ---> is synchronous
+//fs.append()
+//fs.unlink()
+//fs.mkdir()
